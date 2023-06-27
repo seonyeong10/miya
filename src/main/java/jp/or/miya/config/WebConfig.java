a1515e1b2cec7a4f2503442c243f10e5a5390cf6
seonyeong10 <suny951013@naver.com>
2023-06-27 16:13:23 +0900

    PUT CORS 수정

--- src/main/java/jp/or/miya/config/WebConfig.java
+++ src/main/java/jp/or/miya/config/WebConfig.java
@@ -2,6 +2,7 @@ package jp.or.miya.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
+                .allowedMethods(
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
                 .allowedOrigins("http://localhost:3000");
     }
 }
